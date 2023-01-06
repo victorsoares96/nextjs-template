b94b81cfc2e3da2cd64b2373c0d8d2adcc725efe
Victor Soares <vitorsoares96@hotmail.com>
2023-01-06 12:17:12 -0300

    :hammer: devscripts: fix build script

--- next.config.js
+++ next.config.js
@@ -11,10 +11,8 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 const withPwa = require("next-pwa")({
-  pwa: {
-    dest: "public",
-    disable: process.env.NODE_ENV === "development",
-  },
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
 });
 
 module.exports = async (phase) => {
