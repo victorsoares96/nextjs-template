43f3c4063c4eee869551b0ec379db0769a79dc1d
Victor Soares <vitorsoares96@hotmail.com>
2023-01-06 15:35:58 -0300

    :white_check_mark: tests: fix a failing test

--- common/For/tests/index.test.tsx
+++ common/For/tests/index.test.tsx
@@ -9,22 +9,27 @@ import { render } from "@utils/testUtils";
 import For from "../index";
 
 describe("<For />", () => {
-  const Component = render(
-    <For
-      of={[{ name: "Something" }]}
-      noParent={false}
-      ParentComponent={() => <div data-testid="for" />}
-      renderItem={(item, idx: number) => <div key={idx}>{item.name}</div>}
-    />
-  );
-
   it("should render and match the snapshot", () => {
-    const { baseElement } = Component;
+    const { baseElement } = render(
+      <For
+        of={[{ name: "Something" }]}
+        noParent={false}
+        ParentComponent={() => <div data-testid="for" />}
+        renderItem={(item, idx: number) => <div key={idx}>{item.name}</div>}
+      />
+    );
     expect(baseElement).toMatchSnapshot();
   });
 
   it("should contain 1 For component", () => {
-    const { getAllByTestId } = Component;
+    const { getAllByTestId } = render(
+      <For
+        of={[{ name: "Something" }]}
+        noParent={false}
+        ParentComponent={() => <div data-testid="for" />}
+        renderItem={(item, idx: number) => <div key={idx}>{item.name}</div>}
+      />
+    );
     expect(getAllByTestId("for").length).toBe(1);
   });
 });
